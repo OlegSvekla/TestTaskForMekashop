596aaf152cab1af07b1c128037cb2f1f2448f980
olegsvekla993 <olegsvekla993@gmail.com>
2023-01-31 13:44:48 +0300

    add Web MVC project

---
+++ TestTaskForMekashop/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TestTaskForMekashop.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
